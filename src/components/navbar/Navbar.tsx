ac4ef8f4ec366f4376983ccfee49f59b6e40053e
LimaLeo13 <limaleo13@hotmail.com>
2023-11-09 11:29:24 -0300

    Configuração da Home

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full bg-lime-600 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmacia Front end
+
+                    <div className='flex gap-4'>
+                        Categoria
+                        Cadastrar categoria
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
